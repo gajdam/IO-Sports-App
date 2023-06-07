0cc2b44078266b829432f05fad401ccf9f4ef807
gajdam <gajam11@gmail.com>
2023-06-07 12:27:21 +0200

    v10

--- app/src/main/java/com/example/finalproject/ProfileACTIVITY.java
+++ app/src/main/java/com/example/finalproject/ProfileACTIVITY.java
@@ -4,38 +4,71 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
 public class ProfileACTIVITY extends AppCompatActivity {
-    TextView sportName;
-    TextView cityName;
-    TextView time;
+    EditText sportName;
+    EditText cityName;
+    EditText timeName;
+    EditText dateName;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        saveUserPreferences("Football", "New York", "Evening");
+//        saveUserPreferences("Football", "New York", "Evening", "");
 
         Bundle b = getIntent().getExtras();
         String number = b.getString("number");
-        TextView field = (TextView) findViewById(R.id.txtVName);
-        sportName = (TextView) findViewById(R.id.txtSportName);
-        cityName = (TextView) findViewById(R.id.txtCity);
-        time = (TextView) findViewById(R.id.txtTime);
+        TextView field = (TextView) findViewById(R.id.txtNum);
+        sportName = (EditText) findViewById(R.id.edSport);
+        cityName = (EditText) findViewById(R.id.edCity);
+        timeName = (EditText) findViewById(R.id.edTime);
+        dateName = (EditText) findViewById(R.id.edTime);
         loadUserPreferences();
         field.setText(number);
     }
 
-    private void saveUserPreferences(String favoriteSport, String preferredCity, String preferredTime) {
+        public void onClick(View v) {
+            String sport = sportName.getText().toString();
+            if (TextUtils.isEmpty(sport)) {
+                sport = "";
+            }
+
+            String city = cityName.getText().toString();
+            if (TextUtils.isEmpty(city)) {
+                city = "";
+            }
+
+            String time = timeName.getText().toString();
+            if (TextUtils.isEmpty(time)) {
+                time = "";
+            }
+
+            String date = dateName.getText().toString();
+            if (TextUtils.isEmpty(date)) {
+                date = "";
+            }
+
+            saveUserPreferences(sport, city, time, date);
+            Toast.makeText(this, "done", Toast.LENGTH_SHORT).show();
+        }
+
+    private void saveUserPreferences(String favoriteSport, String preferredCity, String preferredTime, String preferredDates) {
         SharedPreferences sharedPreferences = getSharedPreferences("UserPreferences", MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.putString("FavoriteSport", favoriteSport);
         editor.putString("PreferredCity", preferredCity);
         editor.putString("PreferredTime", preferredTime);
+        editor.putString("PreferredDate", preferredDates);
         editor.apply();
     }
 
@@ -51,15 +84,7 @@ public class ProfileACTIVITY extends AppCompatActivity {
 
         sportName.setText(favoriteSport);
         cityName.setText(preferredCity);
-        time.setText(preferredTime);
+        timeName.setText(preferredTime);
+        dateName.setText(preferredTime);
     }
-
-
-
-//    public void wyswietl() {
-//        Bundle b = getIntent().getExtras();
-//        String parametr = b.getString("data");
-//        TextView pole = (TextView) findViewById(R.id.txtView);
-//        pole.setText(parametr);
-//    }
 }
